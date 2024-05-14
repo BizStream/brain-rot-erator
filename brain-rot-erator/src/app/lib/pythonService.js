d1a5efb2391db2c94dab6bfed8d83fd547635afa
5Minty <ardingersam@gmail.com>
2024-05-14 13:34:14 -0400

    built up UI, created API post request to Python

---
+++ brain-rot-erator/src/app/lib/pythonService.js
@@ -0,0 +1,21 @@
+export async function getClips(title, clipLength, fileName) {
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("clipLength", clipLength);
+  formData.append("fileName", fileName);
+
+  try {
+    const response = await fetch("http://localhost:3000/api/clips", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    return await response;
+  } catch (error) {
+    console.error("Error submitting API request:", error);
+  }
+}
--- brain-rot-erator/src/app/page.js
+++ brain-rot-erator/src/app/page.js
@@ -1,10 +1,106 @@
-'use client';
+"use client";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
+import { getClips } from "./lib/pythonService";
 
 export default function Home() {
+  const [title, setTitle] = useState("");
+  const [clipLength, setClipLength] = useState(5);
+  const [fileName, setFileName] = useState("");
+
+  const fileInputRef = useRef(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", title, clipLength);
+    setTitle("");
+    setClipLength(5);
+
+    const answerRes = await getClips(title, clipLength, fileName);
+
+    if (answerRes.status === 200) {
+      console.log("Success");
+    } else {
+      console.log("Error");
+    }
+  };
+
+  const handleAttachClick = (e) => {
+    e.preventDefault();
+    fileInputRef.current.click();
+    console.log("Attach clicked", fileName);
+  };
+
+  const handleFileChange = (files) => {
+    const file = files[0];
+    setFileName(file.name);
+    console.log("File changed", file.name);
+  };
+
   return (
-    <div>
-      <p>Brain Rot-erator</p>
+    <div className="">
+      <div className="flex max-w-900 mx-auto w-full justify-center">
+        <div className="flex flex-col">
+          <p className="flex justify-center my-4 font-bold">Brain Rot-erator</p>
+          <form
+            className="flex flex-col space-y-4"
+            onSubmit={(e) => handleSubmit(e)}
+          >
+            <div className="flex flex-col space-y-4">
+              <div className="flex">
+                <p className="flex items-center px-4">Movie title</p>
+                <input
+                  type="text"
+                  placeholder="Enter movie title here..."
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none text-black"
+                />
+              </div>
+
+              <div className="flex">
+                <button
+                  type="button"
+                  onClick={(e) => handleAttachClick(e)}
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded flex w-fit"
+                >
+                  Attach mp4
+                </button>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={(e) => handleFileChange(e.target.files)}
+                ></input>
+                <p className="flex items-center px-4 text-blue-500">
+                  {fileName}
+                </p>
+              </div>
+
+              <div className="flex">
+                <p className="flex items-center px-4">Clip length (secs)</p>
+                <select
+                  value={clipLength}
+                  onChange={(e) => setClipLength(e.target.value)}
+                  class="border border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none text-black self-center"
+                >
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                </select>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-center"
+              onClick={(e) => handleSubmit(e)}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
